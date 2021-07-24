983c65bc9beb07fcfc58a6189738019fbbd58fc2
Aruna S <aruna.evam@gmail.com>
2021-07-24 00:06:51 +0000

    always use the full input image in VQ disc

--- models/wav2lip.py
+++ models/wav2lip.py
@@ -31,7 +31,7 @@ class Wav2Lip(nn.Module):
 
             nn.Sequential(Conv2d(256, 512, kernel_size=3, stride=2, padding=1),     # 3,3
             Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),),
-            
+
             nn.Sequential(Conv2d(512, 512, kernel_size=3, stride=1, padding=0),     # 1, 1
             Conv2d(512, 512, kernel_size=1, stride=1, padding=0)),])
 
@@ -72,7 +72,7 @@ class Wav2Lip(nn.Module):
             Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),
             Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),), # 24, 24
 
-            nn.Sequential(Conv2dTranspose(320, 128, kernel_size=3, stride=2, padding=1, output_padding=1), 
+            nn.Sequential(Conv2dTranspose(320, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
             Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
             Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),), # 48, 48
 
@@ -82,7 +82,7 @@ class Wav2Lip(nn.Module):
 
         self.output_block = nn.Sequential(Conv2d(80, 32, kernel_size=3, stride=1, padding=1),
             nn.Conv2d(32, 3, kernel_size=1, stride=1, padding=0),
-            nn.Sigmoid()) 
+            nn.Sigmoid())
 
     def forward(self, audio_sequences, face_sequences):
         # audio_sequences = (B, T, 1, 80, 16)
@@ -110,7 +110,7 @@ class Wav2Lip(nn.Module):
                 print(x.size())
                 print(feats[-1].size())
                 raise e
-            
+
             feats.pop()
 
         x = self.output_block(x)
@@ -121,7 +121,7 @@ class Wav2Lip(nn.Module):
 
         else:
             outputs = x
-            
+
         return outputs
 
 class Wav2Lip_disc_qual(nn.Module):
@@ -131,7 +131,7 @@ class Wav2Lip_disc_qual(nn.Module):
         self.face_encoder_blocks = nn.ModuleList([
             nn.Sequential(nonorm_Conv2d(3, 32, kernel_size=7, stride=1, padding=3)), # 48,96
 
-            nn.Sequential(nonorm_Conv2d(32, 64, kernel_size=5, stride=(1, 2), padding=2), # 48,48
+            nn.Sequential(nonorm_Conv2d(32, 64, kernel_size=5, stride=1, padding=2), # 48,48
             nonorm_Conv2d(64, 64, kernel_size=5, stride=1, padding=2)),
 
             nn.Sequential(nonorm_Conv2d(64, 128, kernel_size=5, stride=2, padding=2),    # 24,24
@@ -145,15 +145,12 @@ class Wav2Lip_disc_qual(nn.Module):
 
             nn.Sequential(nonorm_Conv2d(512, 512, kernel_size=3, stride=2, padding=1),     # 3,3
             nonorm_Conv2d(512, 512, kernel_size=3, stride=1, padding=1),),
-            
+
             nn.Sequential(nonorm_Conv2d(512, 512, kernel_size=3, stride=1, padding=0),     # 1, 1
             nonorm_Conv2d(512, 512, kernel_size=1, stride=1, padding=0)),])
 
         self.binary_pred = nn.Sequential(nn.Conv2d(512, 1, kernel_size=1, stride=1, padding=0), nn.Sigmoid())
         self.label_noise = .0
-
-    def get_lower_half(self, face_sequences):
-        return face_sequences[:, :, face_sequences.size(2)//2:]
 
     def to_2d(self, face_sequences):
         B = face_sequences.size(0)
@@ -163,7 +160,6 @@ class Wav2Lip_disc_qual(nn.Module):
     def perceptual_forward(self, false_face_sequences):
         device = false_face_sequences.device
         false_face_sequences = self.to_2d(false_face_sequences)
-        false_face_sequences = self.get_lower_half(false_face_sequences)
 
         false_feats = false_face_sequences
         for f in self.face_encoder_blocks:
@@ -176,7 +172,6 @@ class Wav2Lip_disc_qual(nn.Module):
 
     def forward(self, face_sequences):
         face_sequences = self.to_2d(face_sequences)
-        face_sequences = self.get_lower_half(face_sequences)
 
         x = face_sequences
         for f in self.face_encoder_blocks:
