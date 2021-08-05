2f54b58bcf6141a9dc359b758178941f215d4839
jagnusson <jackmagnusson@gmail.com>
2021-08-05 17:59:06 +0000

    only allow .mp4 paths in evals

--- evaluation/scores_LSE/calculate_scores_real_videos.sh
+++ evaluation/scores_LSE/calculate_scores_real_videos.sh
@@ -1,7 +1,6 @@
 data_dir=$1
-yourfilenames=`ls $1`
+yourfilenames=`ls $1 | grep .mp4`
 score_output_file=$2
-FID_output_dir=$3
 
 if [ -f $score_output_file ]; then
    echo "score file already exists, aborting"
@@ -12,8 +11,4 @@ for eachfile in $yourfilenames
 do
    python run_pipeline.py --videofile $1/$eachfile --reference wav2lip --data_dir tmp_dir
    python calculate_scores_real_videos.py --videofile $1/$eachfile --reference wav2lip --data_dir tmp_dir >> $score_output_file
-   for img in `ls tmp_dir/pytmp/wav2lip/ | grep .jpg`
-   do
-     cp tmp_dir/pytmp/wav2lip/$img $FID_output_dir/${eachfile}${img}
-   done
 done
