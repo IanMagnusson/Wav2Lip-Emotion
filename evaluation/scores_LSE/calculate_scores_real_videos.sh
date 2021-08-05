f6061518eef1d426f3b670f204b33dec26951253
jagnusson <jackmagnusson@gmail.com>
2021-08-05 19:50:11 +0000

    progressbar

--- evaluation/scores_LSE/calculate_scores_real_videos.sh
+++ evaluation/scores_LSE/calculate_scores_real_videos.sh
@@ -1,14 +1,17 @@
 data_dir=$1
 yourfilenames=`ls $1 | grep .mp4`
 score_output_file=$2
+export CUDA_VISIBLE_DEVICES=$3
 
 if [ -f $score_output_file ]; then
    echo "score file already exists, aborting"
    exit 1
 fi
-
+filecount=`ls $1 | grep .mp4 | wc -l`
+echo $filecount
 for eachfile in $yourfilenames
 do
-   python run_pipeline.py --videofile $1/$eachfile --reference wav2lip --data_dir tmp_dir
-   python calculate_scores_real_videos.py --videofile $1/$eachfile --reference wav2lip --data_dir tmp_dir >> $score_output_file
-done
+   python run_pipeline.py --videofile $1/$eachfile --reference wav2lip --data_dir tmp_dir --min_track 25 --min_face_size 25 > /dev/null 2>&1
+   python calculate_scores_real_videos.py --videofile $1/$eachfile --reference wav2lip --data_dir tmp_dir --batch_size 64 >> $score_output_file 2> /dev/null
+   echo processing $eachfile
+done | pv -l -s $filecount > /dev/null
