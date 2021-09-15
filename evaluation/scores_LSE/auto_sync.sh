3641c0fbc2e25448ed0c3f844048b7e83aa9b45c
jagnusson <jackmagnusson@gmail.com>
2021-09-15 18:09:05 +0000

    update name

--- evaluation/scores_LSE/auto_sync.sh
+++ evaluation/scores_LSE/auto_sync.sh
@@ -2,6 +2,6 @@ short_name=$1
 for f in ../../results/mead_results/mead_${short_name}2?_full/
 do
 	base=`echo $f | cut -d "/" -f5`
-	echo "python calculate_scores_LRS.py --data_root /u/ianmag/emotion-synthesis/results/mead_results/$base/ > /u/ianmag/emotion-synthesis/results/mead_results/$base/sync_scores.txt"
-	python calculate_scores_LRS.py --data_root /u/ianmag/emotion-synthesis/results/mead_results/$base/ > /u/ianmag/emotion-synthesis/results/mead_results/$base/sync_scores.txt
+	echo "python calculate_sync_scores.py --data_root /u/ianmag/emotion-synthesis/results/mead_results/$base/ > /u/ianmag/emotion-synthesis/results/mead_results/$base/sync_scores.txt"
+	python calculate_sync_scores.py --data_root /u/ianmag/emotion-synthesis/results/mead_results/$base/ > /u/ianmag/emotion-synthesis/results/mead_results/$base/sync_scores.txt
 done
